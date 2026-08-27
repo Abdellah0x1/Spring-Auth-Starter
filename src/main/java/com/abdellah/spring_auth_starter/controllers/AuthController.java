de77cc10b37a3786ca28f5d1b478548b11ea8dba
abdellah0x1 <abellah.elghennami@usmba.ac.ma>
2026-08-27 22:11:22 +0100

    feat: /api/auth/me done

--- src/main/java/com/abdellah/spring_auth_starter/controllers/AuthController.java
+++ src/main/java/com/abdellah/spring_auth_starter/controllers/AuthController.java
@@ -12,14 +12,12 @@ import com.abdellah.spring_auth_starter.services.AuthService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -62,5 +60,12 @@ public class AuthController {
         ResponseCookie cleanCookie = jwtService.getCleanJwtCookie();
 
         return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cleanCookie.toString()).body(new MessageResponse("You have been logged out"));
+    }
+
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDTO> getMe(){
+        UserDTO userDTO = authService.getCurrentUser();
+        return new ResponseEntity<>(userDTO, HttpStatus.OK);
     }
 }
--- src/main/java/com/abdellah/spring_auth_starter/services/AuthService.java
+++ src/main/java/com/abdellah/spring_auth_starter/services/AuthService.java
@@ -4,13 +4,16 @@ package com.abdellah.spring_auth_starter.services;
 import com.abdellah.spring_auth_starter.entity.User;
 import com.abdellah.spring_auth_starter.enums.USER_ROLE;
 import com.abdellah.spring_auth_starter.exception.APIException;
+import com.abdellah.spring_auth_starter.payload.UserDTO;
 import com.abdellah.spring_auth_starter.repository.UserRepository;
 import com.abdellah.spring_auth_starter.security.requests.LoginRequest;
 import com.abdellah.spring_auth_starter.security.requests.RegistrationRequest;
+import com.abdellah.spring_auth_starter.security.services.UserDetailsImpl;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -47,8 +50,19 @@ public class AuthService {
     }
 
     public Authentication login(LoginRequest loginRequest) throws  AuthenticationException {
-
         return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+    }
 
+    public UserDTO getCurrentUser(){
+        UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        UserDTO userDTO = new UserDTO();
+        userDTO.setFirstName(userDetails.getFirstName());
+        userDTO.setLastName(userDetails.getLastName());
+        userDTO.setEmail(userDetails.getEmail());
+        String role = userDetails.getAuthorities().stream().findFirst().orElseThrow().getAuthority();
+        userDTO.setRole(USER_ROLE.valueOf(role));
+
+        return userDTO;
     }
 }
